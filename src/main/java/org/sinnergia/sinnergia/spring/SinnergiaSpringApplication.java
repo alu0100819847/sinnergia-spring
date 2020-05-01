c7d943b54239c7c5c4d353b0f39e6619c0316082
Carlos Castro García <alu0100819847@ull.edu.es>
2020-05-01 19:03:32 +0200

    Init

---
+++ src/main/java/org/sinnergia/sinnergia/spring/SinnergiaSpringApplication.java
@@ -0,0 +1,13 @@
+package org.sinnergia.sinnergia.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinnergiaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinnergiaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sinnergia/sinnergia/spring/SinnergiaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sinnergia.sinnergia.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinnergiaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
